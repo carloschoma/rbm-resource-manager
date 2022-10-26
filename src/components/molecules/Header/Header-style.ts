b91680d0790e1a6f870350c1c559805fb19fafbd
romneycf <romneycf@gmail.com>
2022-10-26 13:01:16 -0300

    Header css

---
+++ src/components/molecules/Header/Header-style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  position: absolute;
+  width: 1440px;
+  height: 76px;
+  left: 0px;
+  top: 0px;
+  background: #FFFFFF;
+`;
+
--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,11 +1,15 @@
 import * as Styled from "./Header-style";
 import React from 'react'
+import Logoimg from "components/particules/Logoimg/Logoimg";
 
 export interface  HeaderPropsInterface
   extends React.HTMLAttributes<HTMLElement> {}
 
 export default function Header({children, className, ...rest}: HeaderPropsInterface) {
     return (
-        <Styled.StyledDiv className={className}>{children}'HEADER TESTE'</Styled.StyledDiv>
+        <Styled.StyledDiv className={className}>
+          <Logoimg />
+          {children}
+        </Styled.StyledDiv>
     )
 }
