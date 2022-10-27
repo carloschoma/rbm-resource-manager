20dc8989adb5933058a4e609c53e2f88932a2978
Nata_Rodrigues <natarodrigues2019@outlook.com>
2022-10-27 20:10:12 -0300

    feat:button

--- src/components/molecules/Login/Login.tsx
+++ src/components/molecules/Login/Login.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import Text from 'components/particules/Text/Text';
-import * as Styled from'./Login-style';
-import Logoimg from 'components/particules/Logoimg/Logoimg';
-import SpanInput from 'components/atoms/SpanInput/SpanInput';
-import Button from 'components/particules/Button/Button';
-
-
+import React from "react";
+import Text from "components/particules/Text/Text";
+import * as Styled from "./Login-style";
+import Logoimg from "components/particules/Logoimg/Logoimg";
+import SpanInput from "components/atoms/SpanInput/SpanInput";
+import Button from "components/particules/Button/Button";
 
 export default function Login() {
-    return (
-        <Styled.StyledLogin>
-        <Logoimg/>
-       <Text tag={'h1'}>Entrar na minha conta</Text>
-       <Text tag={'p'} weight="400">Entrar na minha conta</Text>
-        <SpanInput spanText='E-mail' type='text' placeholder=' email@rbmweb.com.br'/>
-        <SpanInput spanText='Senha' type='password' placeholder=' Digite a sua senha'/>
-   <Button>Entar</Button>
-        </Styled.StyledLogin>
-    );  
-} 
+  return (
+    <Styled.StyledLogin>
+      <Logoimg />
+      <Text tag={"h1"}>Entrar na minha conta</Text>
+      <Text tag={"p"} weight="400">
+        Entrar na minha conta
+      </Text>
+      <SpanInput
+        spanText="E-mail"
+        type="text"
+        placeholder=" email@rbmweb.com.br"
+      />
+      <SpanInput
+        spanText="Senha"
+        type="password"
+        placeholder=" Digite a sua senha"
+      />
+      <Button>Entar</Button>
+    </Styled.StyledLogin>
+  );
+}
