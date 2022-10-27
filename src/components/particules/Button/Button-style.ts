1f52ab9276845d6b88fdb0d46b3bf26cbb6a6162
romneycf <romneycf@gmail.com>
2022-10-27 20:03:20 -0300

    button

---
+++ src/components/particules/Button/Button-style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+    all: unset;
+    cursor: pointer;
+    color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background: #0092D4;
+    border-radius: 6px;
+    width: 410px;
+    height: 58px;
+`;
+
