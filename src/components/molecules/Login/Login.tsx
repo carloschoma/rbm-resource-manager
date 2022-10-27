b8c8e2b55f09ec20af151b281510b1580a32d14c
Nata_Rodrigues <natarodrigues2019@outlook.com>
2022-10-27 20:10:01 -0300

    feat/button

--- src/components/molecules/Login/Login.tsx
+++ src/components/molecules/Login/Login.tsx
@@ -3,6 +3,7 @@ import Text from 'components/particules/Text/Text';
 import * as Styled from'./Login-style';
 import Logoimg from 'components/particules/Logoimg/Logoimg';
 import SpanInput from 'components/atoms/SpanInput/SpanInput';
+import Button from 'components/particules/Button/Button';
 
 
 
@@ -14,7 +15,7 @@ export default function Login() {
        <Text tag={'p'} weight="400">Entrar na minha conta</Text>
         <SpanInput spanText='E-mail' type='text' placeholder=' email@rbmweb.com.br'/>
         <SpanInput spanText='Senha' type='password' placeholder=' Digite a sua senha'/>
-   
+   <Button>Entar</Button>
         </Styled.StyledLogin>
     );  
 } 
